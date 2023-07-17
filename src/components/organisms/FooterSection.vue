3e10bdc0370a2d6e13075edc38d119d8502a245b
thundermiracle <cissoliu@gmail.com>
2023-07-17 17:43:56 +0900

    feat: add footer

---
+++ src/components/organisms/FooterSection.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import TwitterIcon from '@/components/atoms/TwitterIcon.vue';
+import FacebookIcon from '@/components/atoms/FacebookIcon.vue';
+import LinkedinIcon from '@/components/atoms/LinkedinIcon.vue';
+import GithubIcon from '@/components/atoms/GithubIcon.vue';
+import MediumIcon from '@/components/atoms/MediumIcon.vue';
+
+defineProps<{
+  copyright: string;
+  privacyHref: string;
+  privacyText: string;
+  termsHref: string;
+  termsText: string;
+  social?: {
+    twitter?: string;
+    facebook?: string;
+    linkedin?: string;
+    github?: string;
+    medium?: string;
+  };
+}>();
+</script>
+
+<template>
+  <footer class="py-2">
+    <div class="container mx-auto">
+      <div class="flex items-center flex-wrap text-center">
+        <div class="md:basis-1/3 basis-1/1 md:text-right">{{ copyright }}</div>
+        <div class="my-2 md:basis-1/3 basis-1/1 text-center">
+          <TwitterIcon v-if="social?.twitter" :userName="social?.twitter" />
+          <FacebookIcon v-if="social?.facebook" :userName="social?.facebook" />
+          <LinkedinIcon v-if="social?.linkedin" :userName="social?.linkedin" />
+          <GithubIcon v-if="social?.github" :userName="social?.github" />
+          <MediumIcon v-if="social?.medium" :userName="social?.medium" />
+        </div>
+        <div class="md:basis-1/3 basis-1/1 md:text-left">
+          <a class="mr-3 text-primary" :href="privacyHref">
+            {{ privacyText }}
+          </a>
+          <a class="text-primary" :href="termsHref">
+            {{ termsText }}
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ import TimelineSection from '@/components/organisms/TimelineSection.vue';
 import TeamSection from '@/components/organisms/TeamSection.vue';
 import ClientsSection from '@/components/organisms/ClientsSection.vue';
 import ContactSection from '@/components/organisms/ContactSection.vue';
+import FooterSection from '@/components/organisms/FooterSection.vue';
 
 import TopDataJSON from 'contents/en/topData.json';
 import ServicesDataJson from 'contents/en/servicesData.json';
@@ -14,6 +15,7 @@ import TimelineDataJson from 'contents/en/timelineData.json';
 import TeamDataJson from 'contents/en/teamData.json';
 import ClientsDataJson from 'contents/en/clientsData.json';
 import ContactDataJson from 'contents/en/contactData.json';
+import FooterDataJson from 'contents/en/footerData.json';
 </script>
 
 <template>
@@ -25,5 +27,6 @@ import ContactDataJson from 'contents/en/contactData.json';
     <TeamSection :="TeamDataJson" />
     <ClientsSection :="ClientsDataJson" />
     <ContactSection :="ContactDataJson" />
+    <FooterSection :="FooterDataJson" />
   </main>
 </template>
